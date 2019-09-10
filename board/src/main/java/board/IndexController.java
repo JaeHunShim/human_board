a380e92de82aff5c5eab7a5d1c0f21f66e59812e
JaeHunShim <jaehuniya@gmail.com>
2019-09-10 17:52:12 +0900

    indexController rollback

--- board/src/main/java/board/IndexController.java
+++ board/src/main/java/board/IndexController.java
@@ -9,21 +9,7 @@ public class IndexController {
 //
 	@RequestMapping("/")
 	public String index(ModelMap model) throws Exception {
-<<<<<<< HEAD
-		1111111111111111111111
-		1111111111111111111111
-		222222222222222222222
-		222222222222222222222
-		222222222222222222222
-		44444444444444444444444444444
-		1111111111111111111111
-		1111111111111111111111
-		1111111111111111111111
-		1111111111111111111111
-
-=======
 		
->>>>>>> parent of 4d89f0e... second modify
 		model.addAttribute("name","jaehun");
 		return "index";
 	}
